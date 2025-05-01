794175ea5817e522d125e60571a991ee3973a8b2
abdullah20020 <105291090+abdullah20020@users.noreply.github.com>
2025-05-02 00:26:19 +0300

    COMMENT ALL NEW

--- Rawy/Controllers/UserInterestController.cs
+++ Rawy/Controllers/UserInterestController.cs
@@ -1,70 +1,70 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using core.Models;
-using System.Security.Claims;
-using Repsotiry.Data;
-using Repsotiry.GenaricReposiory;
-using Rawy.Dtos;
-using AutoMapper;
-using Services;
+﻿//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+//using core.Models;
+//using System.Security.Claims;
+//using Repsotiry.Data;
+//using Repsotiry.GenaricReposiory;
+//using Rawy.Dtos;
+//using AutoMapper;
+//using Services;
 
-namespace Rawy.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    //[Authorize]
-    public class UserInterestController : ControllerBase
-    {
-
-
-        private readonly RawyDbcontext rawyDbcontext;
-        private readonly IMapper mapper;
-        private readonly CsvGeneratorService csvService;
-
-        public UserInterestController(RawyDbcontext rawyDbcontext, IMapper mapper, CsvGeneratorService csvService)
-        {
-
-            this.rawyDbcontext = rawyDbcontext;
-            this.mapper = mapper;
-            this.csvService = csvService;
-        }
+//namespace Rawy.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    //[Authorize]
+//    public class UserInterestController : ControllerBase
+//    {
 
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<UserInterestbook>>> GetMyInterests()
-        {
-            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+//        private readonly RawyDbcontext rawyDbcontext;
+//        private readonly IMapper mapper;
+//        private readonly CsvGeneratorService csvService;
 
-            var interests = await rawyDbcontext.UserInterests
-                .Include(u => u.Book)
-                .Where(i => i.UserId == userId)
-                .ToListAsync();
-            var mappeing = mapper.Map<IEnumerable<UserInterestbook>, IEnumerable<UserInterestDtos>>(interests);
+//        public UserInterestController(RawyDbcontext rawyDbcontext, IMapper mapper, CsvGeneratorService csvService)
+//        {
 
-            var memoryStream = csvService.GenerateCsvInMemory(mappeing);
-
-            await csvService.SendCsvToPythonServerAsync(memoryStream);
-
-            return Ok(mappeing);
-        }
+//            this.rawyDbcontext = rawyDbcontext;
+//            this.mapper = mapper;
+//            this.csvService = csvService;
+//        }
 
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteInterest(int id)
-        {
-            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+//        [HttpGet]
+//        public async Task<ActionResult<IEnumerable<UserInterestbook>>> GetMyInterests()
+//        {
+//            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
-            var interest = await rawyDbcontext.UserInterests
-                .FirstOrDefaultAsync(i => i.Id == id && i.UserId == userId);
+//            var interests = await rawyDbcontext.UserInterests
+//                .Include(u => u.Book)
+//                .Where(i => i.UserId == userId)
+//                .ToListAsync();
+//            var mappeing = mapper.Map<IEnumerable<UserInterestbook>, IEnumerable<UserInterestDtos>>(interests);
 
-            if (interest == null)
-                return NotFound();
+//            var memoryStream = csvService.GenerateCsvInMemory(mappeing);
 
-            rawyDbcontext.UserInterests.Remove(interest);
-            await rawyDbcontext.SaveChangesAsync();
+//            await csvService.SendCsvToPythonServerAsync(memoryStream);
 
-            return NoContent();
-        }
-    }
-}
+//            return Ok(mappeing);
+//        }
+
+
+//        [HttpDelete("{id}")]
+//        public async Task<IActionResult> DeleteInterest(int id)
+//        {
+//            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+//            var interest = await rawyDbcontext.UserInterests
+//                .FirstOrDefaultAsync(i => i.Id == id && i.UserId == userId);
+
+//            if (interest == null)
+//                return NotFound();
+
+//            rawyDbcontext.UserInterests.Remove(interest);
+//            await rawyDbcontext.SaveChangesAsync();
+
+//            return NoContent();
+//        }
+//    }
+//}
--- Services/CsvGeneratorService.cs
+++ Services/CsvGeneratorService.cs
@@ -1,49 +1,49 @@
-﻿using CsvHelper;
-using System;
-using System.Collections.Generic;
-using System.Formats.Asn1;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using CsvHelper;
+//using System;
+//using System.Collections.Generic;
+//using System.Formats.Asn1;
+//using System.Globalization;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Services
-{
-    public class CsvGeneratorService
-    {
-        public MemoryStream GenerateCsvInMemory<T>(IEnumerable<T> records)
-        {
-            var memoryStream = new MemoryStream();
-            using (var streamWriter = new StreamWriter(memoryStream, Encoding.UTF8, leaveOpen: true))
-            using (var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture))
-            {
-                csvWriter.WriteRecords(records);
-            }
-            memoryStream.Position = 0;
-            return memoryStream;
-        }
+//namespace Services
+//{
+//    public class CsvGeneratorService
+//    {
+//        public MemoryStream GenerateCsvInMemory<T>(IEnumerable<T> records)
+//        {
+//            var memoryStream = new MemoryStream();
+//            using (var streamWriter = new StreamWriter(memoryStream, Encoding.UTF8, leaveOpen: true))
+//            using (var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture))
+//            {
+//                csvWriter.WriteRecords(records);
+//            }
+//            memoryStream.Position = 0;
+//            return memoryStream;
+//        }
 
-        public async Task SendCsvToPythonServerAsync(MemoryStream csvStream)
-        {
-            using var client = new HttpClient();
+//        public async Task SendCsvToPythonServerAsync(MemoryStream csvStream)
+//        {
+//            using var client = new HttpClient();
 
-            var content = new MultipartFormDataContent();
-            csvStream.Position = 0;
-            var fileContent = new StreamContent(csvStream);
-            fileContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("text/csv");
+//            var content = new MultipartFormDataContent();
+//            csvStream.Position = 0;
+//            var fileContent = new StreamContent(csvStream);
+//            fileContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("text/csv");
 
-            content.Add(fileContent, "file", "user_interests.csv");
-            var response = await client.PostAsync("/upload", content);
+//            content.Add(fileContent, "file", "user_interests.csv");
+//            var response = await client.PostAsync("/upload", content);
 
 
-            if (response.IsSuccessStatusCode)
-            {
-                Console.WriteLine("CSV uploaded successfully to Python server!");
-            }
-            else
-            {
-                Console.WriteLine($"Failed to upload CSV. Status: {response.StatusCode}");
-            }
-        }
-    }
-}
+//            if (response.IsSuccessStatusCode)
+//            {
+//                Console.WriteLine("CSV uploaded successfully to Python server!");
+//            }
+//            else
+//            {
+//                Console.WriteLine($"Failed to upload CSV. Status: {response.StatusCode}");
+//            }
+//        }
+//    }
+//}
